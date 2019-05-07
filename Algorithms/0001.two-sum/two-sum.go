24c657c234c5b590f8373dfcd1f9aaffe9a5ff23
TonyWangBit <plantpark.net@gmail.com>
2019-05-07 18:32:52 +0800

    finish 1.two sum

--- Algorithms/0001.two-sum/two-sum.go
+++ Algorithms/0001.two-sum/two-sum.go
@@ -1,22 +1,15 @@
 package problem0001
 
 func twoSum(nums []int, target int) []int {
-	// index 负责保存map[整数]整数的序列号
+
+	// create an map to store item and index
 	index := make(map[int]int, len(nums))
-
-	// 通过 for 循环，获取b的序列号
+	// loop all the items in there and store the item as key, index as value into map. caculate the result with target
 	for i, b := range nums {
-		// 通过查询map，获取a = target - b的序列号
 		if j, ok := index[target-b]; ok {
-			// ok 为 true
-			// 说明在i之前，存在 nums[j] == a
 			return []int{j, i}
-			// 注意，顺序是j，i
 		}
-
-		// 把b和i的值，存入map
 		index[b] = i
 	}
-
 	return nil
 }
